4648c67a0f0d78bf6bf8b79773f41a63e746e955
SeoyoungChoii <sychoi@forcs.com>
2021-10-15 15:05:24 +0900

    파비콘 이미지 수정/ theme 수정

--- conf.py
+++ conf.py
@@ -74,7 +74,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'press'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -87,8 +87,7 @@ html_theme = 'press'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_favicon = 'C:\docbook\eformsignkr.github.io\images\\favicon.png'
-
+html_favicon = 'C:\docbook\eformsignkr.github.io\images\\favicon.ico'
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
