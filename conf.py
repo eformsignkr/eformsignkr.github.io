df2c949cf35d75a22abc4f0ea8f0a231d6ca7286
SeoyoungChoii <sychoi@forcs.com>
2021-10-15 15:54:40 +0900

    theme 변경

--- conf.py
+++ conf.py
@@ -87,7 +87,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_favicon = 'favicon.ico'
+html_favicon = 'C:\docbook\eformsignkr.github.io\images\\favicon.ico'
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
@@ -177,4 +177,4 @@ html_theme_options = {
     'logo_only': True,
     # ...
 }
-html_logo = "C:\docbook\eformsignjp.github.io\images\\forcs.png"
+html_logo = "C:\docbook\eformsignkr.github.io\images\\forcs_b.png"
