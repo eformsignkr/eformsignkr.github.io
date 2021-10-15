cd752968f66f1c920402cc48e3daddc3d4833361
SeoyoungChoii <sychoi@forcs.com>
2021-10-15 15:12:35 +0900

    파비콘 수정

--- conf.py
+++ conf.py
@@ -87,7 +87,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_favicon = 'C:\docbook\eformsignkr.github.io\images\\favicon.ico'
+html_favicon = 'favicon.ico'
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
