38a4f0e14fd5ce079e3fc21a38bca6ae6728ec5b
sychoii <sychoi@forcs.com>
2023-03-06 10:52:09 +0900

    법인 범용 용어/이미지 수정

--- conf.py
+++ conf.py
@@ -39,7 +39,11 @@ release = '2021.10.22'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+#'sphinxcontrib.redoc',
+    'sphinx_code_tabs',
+    'sphinx_rtd_theme',
 ]
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
